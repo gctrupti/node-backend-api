e5d5e3a580e92900f3ed47259bd2433a41cac605
gctrupti <gctrupti.amcec@gmail.com>
2026-01-15 03:32:43 +0530

    Fix port handling for Render

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ const userRoutes = require("./routes/users");
 // use user routes
 app.use("/users", userRoutes);
 
-// start server
-app.listen(3000, () => {
-  console.log("Express server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
